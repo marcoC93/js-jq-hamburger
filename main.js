861d4bcf3bc4137b2fef57d95bb775d94bea694b
Marco <marco.chianese93@gmail.com>
2020-10-26 16:49:43 +0100

    solo commenti

---
+++ main.js
@@ -0,0 +1,11 @@
+// devo far in modo che al click sull' icona dell'hamburger menu esso compaia
+
+
+
+//devo applicare una funzione al click sulla "i"ovvero l'icona ma la classe che verra modificata é .hamburger-menu la quale non dovra piu avere il display none ma bensi il display: block
+
+
+$( "i").click function() {
+    $(".hamburger-menu").css( { display: block ;
+    }
+)};
